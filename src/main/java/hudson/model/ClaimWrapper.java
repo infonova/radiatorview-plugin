fbc28a744abdfb88f7aa269d7452c1824017841b
seder <stefan.eder@infonova.com>
2015-01-09 11:26:52 +0100

    [INFRABRBA-723] improved recursive folder handling (copied from ListView)

--- src/main/java/hudson/model/ClaimWrapper.java
+++ src/main/java/hudson/model/ClaimWrapper.java
@@ -12,49 +12,48 @@ import org.jfree.util.Log;
  */
 public class ClaimWrapper {
 
-	private ClaimBuildAction claimBuildAction;
+    private ClaimBuildAction claimBuildAction;
 
-	private ClaimWrapper(ClaimBuildAction claimBuildAction) {
-		this.claimBuildAction = claimBuildAction;
-	}
+    private ClaimWrapper(ClaimBuildAction claimBuildAction) {
+        this.claimBuildAction = claimBuildAction;
+    }
 
-	/**
-	 * Returns ClaimWrapper containing the claim for the specified run
-	 * if there is a single ClaimBuildAction for the run. Returns null
-	 * otherwise.
-	 *
-	 * @param run
-	 * @return null if no single ClaimBuildAction for the run param.
-	 */
-	static public ClaimWrapper builder(Run<?, ?> run) {
-		ClaimBuildAction claimForRun = getClaimForRun(run);
-		if (claimForRun == null) {
-			return null;
-		}
-		return new ClaimWrapper(claimForRun);
-	}
+    /**
+     * Returns ClaimWrapper containing the claim for the specified run
+     * if there is a single ClaimBuildAction for the run. Returns null
+     * otherwise.
+     *
+     * @param run
+     * @return null if no single ClaimBuildAction for the run param.
+     */
+    static public ClaimWrapper builder(Run<?, ?> run) {
+        ClaimBuildAction claimForRun = getClaimForRun(run);
+        if (claimForRun == null) {
+            return null;
+        }
+        return new ClaimWrapper(claimForRun);
+    }
 
-	static private ClaimBuildAction getClaimForRun(Run<?, ?> run) {
-		ClaimBuildAction claimAction = null;
-		List<ClaimBuildAction> claimActionList = run
-				.getActions(ClaimBuildAction.class);
-		if (claimActionList.size() == 1) {
-			claimAction = claimActionList.get(0);
-		} else if (claimActionList.size() > 1) {
-			Log.warn("Multiple ClaimBuildActions found for job ");
-		}
-		return claimAction;
-	}
+    static private ClaimBuildAction getClaimForRun(Run<?, ?> run) {
+        ClaimBuildAction claimAction = null;
+        List<ClaimBuildAction> claimActionList = run.getActions(ClaimBuildAction.class);
+        if (claimActionList.size() == 1) {
+            claimAction = claimActionList.get(0);
+        } else if (claimActionList.size() > 1) {
+            Log.warn("Multiple ClaimBuildActions found for job ");
+        }
+        return claimAction;
+    }
 
-	public boolean isClaimed() {
-		return claimBuildAction.isClaimed();
-	}
+    public boolean isClaimed() {
+        return claimBuildAction.isClaimed();
+    }
 
-	public String getReason() {
-		return claimBuildAction.getReason();
-	}
+    public String getReason() {
+        return claimBuildAction.getReason();
+    }
 
-	public String getClaimedByName() {
-		return claimBuildAction.getClaimedByName();
-	}
+    public String getClaimedByName() {
+        return claimBuildAction.getClaimedByName();
+    }
 }
